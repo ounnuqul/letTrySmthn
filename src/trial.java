9c331f3352b1736d53a0a3e6adec9e51d3db9d39
Oun Nuqul <oun.nuqul@mail.utoronto.ca>
2018-03-04 18:34:36 -0500

    added shit

--- src/trial.java
+++ src/trial.java
@@ -1,3 +1,14 @@
 public class trial {
 
+    public String name;
+    public int ID;
+    private static int newestID = 0;
+
+    public trial(String n){
+        this.name = n;
+        this.ID = newestID;
+        newestID++;
+    }
+
+
 }
